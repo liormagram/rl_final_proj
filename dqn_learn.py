a384f2489fa53085a5dff460c6d8c11c4a265a88
Lior <lior.magram10@gmail.com>
2020-06-15 22:13:58 +0300

    fixing last_obs

--- dqn_learn.py
+++ dqn_learn.py
@@ -189,8 +189,8 @@ def dqn_learing(
         if rand < exploration.value(t) or t == 0:
             action = np.random.randint(num_actions)
         else:
-            last_obs = Variable(torch.tensor(last_obs)).data
-            out_actions_vals = Q(last_obs.unsqueeze(0).float())
+            last_obs = Variable(torch.tensor(last_obs).float()).data
+            out_actions_vals = Q(last_obs.unsqueeze(0))
             action = torch.argmax(out_actions_vals).detach().numpy()
         obs, reward, done, info = env.step(action)
         if done:
