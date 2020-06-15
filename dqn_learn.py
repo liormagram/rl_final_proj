467181df070ef1d8f36e1b40954f91accdb26853
Lior <lior.magram10@gmail.com>
2020-06-15 22:35:57 +0300

    fixing last_obs

--- dqn_learn.py
+++ dqn_learn.py
@@ -249,7 +249,7 @@ def dqn_learing(
             mask = Variable(torch.ones(done_mask.shape) - torch.tensor(done_mask)).data
 
             pred_batch = Q(obs_batch.data.cuda().float())
-            Q_values = pred_batch.gather(1, act_batch.data.unsqueeze(1).long()).squeeze()
+            Q_values = pred_batch.gather(1, act_batch.data.unsqueeze(1).cuda().long()).squeeze()
             masked_Q_values = Q_values * mask
 
 
