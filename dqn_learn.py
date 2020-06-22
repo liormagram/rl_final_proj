cca47fe2a76741688f33ef908b05382a2310eb83
Lior <lior.magram10@gmail.com>
2020-06-22 21:41:24 +0300

    fixing cuda problems

--- dqn_learn.py
+++ dqn_learn.py
@@ -248,7 +248,10 @@ def dqn_learing(
             #3.b
             mask = Variable(torch.ones(done_mask.shape) - torch.tensor(done_mask)).data
 
-            pred_batch = Q(obs_batch.data.float())
+            print(obs_batch.dtype)
+            obs_batch = obs_batch.data.float()
+            print(obs_batch.dtype)
+            pred_batch = Q(obs_batch)
             Q_values = pred_batch.gather(1, act_batch.data.unsqueeze(1).long()).squeeze()
             masked_Q_values = Q_values * mask
 
