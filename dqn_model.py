345c33f2a29abd947f64d31327d3f9f574e544d0
Lior <lior.magram10@gmail.com>
2020-07-04 01:25:11 +0300

    adding prints

--- dqn_model.py
+++ dqn_model.py
@@ -12,16 +12,20 @@ class DQN(nn.Module):
             num_actions: number of action-value to output, one-to-one correspondence to action in game.
         """
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+        self.conv1 = nn.Conv2d(in_channels, 32, kernel_size=3, stride=2)
+        self.conv1a = nn.Conv2d(32, 32, kernel_size=3, stride=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=2)
+        self.conv3a = nn.Conv2d(64, 64, kernel_size=3, stride=1)
         self.fc4 = nn.Linear(7 * 7 * 64, 512)
         self.fc5 = nn.Linear(512, num_actions)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
+        x = F.relu(self.conv1a(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
+        x = F.relu(self.conv3a(x))
         x = F.relu(self.fc4(x.view(x.size(0), -1)))
         return self.fc5(x)
 
